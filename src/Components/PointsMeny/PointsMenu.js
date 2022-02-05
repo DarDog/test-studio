0bf1b81899941040ea25a525f0d8dac2ba17b7e6
DarDog <samediacc666@gmial.com>
2022-02-05 23:15:59 +1100

    Feat: Add PointsMenu component

---
+++ src/Components/PointsMeny/PointsMenu.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './PointsMenu.css';
+
+const PointsMenu = ({ onClicl }) => {
+  return (
+    <button className='points__menu'/>
+  );
+};
+
+export default PointsMenu;
