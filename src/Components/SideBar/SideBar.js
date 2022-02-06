9001cee0a0bd2fe8eca2e21449af065daae4a4b0
DarDog <samediacc666@gmial.com>
2022-02-06 15:18:09 +1100

    Feat: Update layout for short state SideBar component

--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SideBar.css';
 import { NavLink } from "react-router-dom";
 import BurgerMenu from "../BurgerMenu/BurgerMenu";
@@ -7,57 +7,61 @@ import { MY_FACE, NOTIFICATIONS } from "../../constants/constants";
 import PointsMenu from "../PointsMeny/PointsMenu";
 
 const SideBar = () => {
-  const setActive = ({ isActive }) => 'nav__link ' + ( isActive ? 'nav__link_active' : '' );
+  const [ isShort, setIsShort ] = useState(false)
+  const setActive = ({ isActive }) => `nav__link ${isShort && 'nav__link_short'}` + ( isActive ? ' nav__link_active' : '' );
 
+  const handleSizeChange = () => {
+    setIsShort(!isShort);
+  }
 
   return (
-    <nav className='nav'>
+    <nav className={ `nav ${isShort && 'nav_short'}` }>
       <div className='nav__header'>
-        <BurgerMenu/>
-        <Logo/>
+        <BurgerMenu onClick={handleSizeChange}/>
+        { isShort || <Logo /> }
       </div>
-      <div className='nav__menu'>
+      <div className={ `nav__menu ${ isShort && 'nav__menu_short' }` }>
         <div className='nav__container'>
           <form className='nav__form'>
-            <input type="text" className='nav__search' placeholder='Search'/>
+            <input type={ isShort ? 'button' : 'text' } className={ `nav__search ${ isShort && 'nav__search_short' }` } placeholder={ isShort ? '' : 'Search' }/>
           </form>
           <ul className='nav__list'>
             <li className='list__item'>
               <NavLink to='/' className={ setActive }>
                 <div className='nav__link-icon nav__link-icon_type_main'/>
-                Главная
+                <p className={ `nav__link-text ${ isShort && 'nav__link-text_short' }` }>Главная</p>
               </NavLink>
             </li>
             <li className='list__item'>
               <NavLink to='/inbox' className={ setActive }>
                 <div className='nav__link-icon nav__link-icon_type_inbox'/>
-                Входящие
-                { NOTIFICATIONS > 0 && <div className='nav__link-notifications'>{ NOTIFICATIONS }</div>}
+                <p className={ `nav__link-text ${ isShort && 'nav__link-text_short' }` }>Входящие</p>
+                { NOTIFICATIONS && !isShort > 0 && <div className='nav__link-notifications'>{ NOTIFICATIONS }</div>}
               </NavLink>
             </li>
             <li className='list__item'>
               <NavLink to='/todo' className={ setActive }>
                 <div className='nav__link-icon nav__link-icon_type_todo'/>
-                Задачи
+                <p className={ `nav__link-text ${ isShort && 'nav__link-text_short' }` }>Задачи</p>
               </NavLink>
             </li>
             <li className='list__item'>
               <NavLink to='/activity' className={ setActive }>
                 <div className='nav__link-icon nav__link-icon_type_activity'/>
-                Активность
+                <p className={ `nav__link-text ${ isShort && 'nav__link-text_short' }` }>Активность</p>
               </NavLink>
             </li>
             <li className='list__item'>
               <NavLink to='/team' className={ setActive }>
                 <div className='nav__link-icon nav__link-icon_type_team'/>
-                Команда
+                <p className={ `nav__link-text ${ isShort && 'nav__link-text_short' }` }>Команда</p>
               </NavLink>
             </li>
           </ul>
         </div>
         <div className='nav__container'>
-          <p className='nav__folder'>Проекты</p>
-          <ul className='nav__list'>
+          <p className={ `nav__folder ${ isShort && 'nav__folder' }` }>{ isShort ? '' : 'Проекты' }</p>
+          <ul className={ `nav__list ${ isShort && 'nav__list_short' }` }>
             <li className='list__item'>
               <NavLink to='/folder/design' className={ setActive }>
                 <div className='list__point'/>
@@ -77,18 +81,20 @@ const SideBar = () => {
               </NavLink>
             </li>
           </ul>
-          <button className='nav__add-button'>Создать проект</button>
+          <button className={ `nav__add-button ${ isShort && 'nav__add-button_short' }` }>
+            <p className={ `nav__button-text ${ isShort && 'nav__button-text_short' }` }>Создать проект</p>
+          </button>
         </div>
       </div>
-      <div className='nav__footer'>
+      <div className={ `nav__footer ${ isShort && 'nav__footer_short' }` }>
         <div className='nav__user'>
           <img src={ MY_FACE } alt="Ваш аватар" className='nav__avatar'/>
-          <div className='nav__user-info'>
+          <div className={ `nav__user-info ${ isShort && 'nav__user-info_short' }` }>
             <p className='nav__user-name'>Владислав Субботин</p>
             <p className='nav__user-job'>Фронтенд разработчик</p>
           </div>
         </div>
-        <PointsMenu />
+        { isShort ? '' : <PointsMenu />}
       </div>
     </nav>
   );
