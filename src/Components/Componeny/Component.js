849691cb35b3ab42646129331f2d8470e9de8bc8
DarDog <samediacc666@gmial.com>
2022-02-06 16:38:31 +1100

    Feat: update layout

--- src/Components/Componeny/Component.js
+++ src/Components/Componeny/Component.js
@@ -4,14 +4,6 @@ import './Component.css'
 const Component = () => {
   return (
     <section className='example-page'>
-      <article className='example-page__header'>
-        <ul className='example-page__list'>
-          <li className='example-page__item'/>
-          <li className='example-page__item'/>
-          <li className='example-page__item'/>
-        </ul>
-        <button className='example-page__button'/>
-      </article>
       <article className='example-page__content'>
         <h1 className='example-page__title'>Пример возможной страницы</h1>
         <ul className='example-page__content-list'>
--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -8,6 +8,14 @@ const Layout = () => {
     <main className='main'>
       <SideBar />
       <div className='content'>
+        <article className='example-page__header'>
+          <ul className='example-page__list'>
+            <li className='example-page__item'/>
+            <li className='example-page__item'/>
+            <li className='example-page__item'/>
+          </ul>
+          <button className='example-page__button'/>
+        </article>
       { <Outlet /> }
       </div>
     </main>
