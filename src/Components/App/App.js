821ef09f1111585bf0ea3fbada9d597afef98461
DarDog <samediacc666@gmial.com>
2022-02-06 18:49:50 +1100

    update for gh-pages

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import Layout from "../Layout/Layout";
 import Component from "../Componeny/Component";
 
-const App = () => {
+function App () {
   return (
-    <Routes>
-      <Route path='/' element={ <Layout/>}>
-        <Route index element={ <Component /> }/>
-        <Route path='inbox' element={ <Component /> }/>
-        <Route path='todo' element={ <Component /> }/>
-        <Route path='activity' element={ <Component /> }/>
-        <Route path='team' element={ <Component /> }/>
-        <Route path='folder/:name' element={ <Component /> }/>
-      </Route>
-    </Routes>
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={ <Layout/> }>
+          <Route index element={ <Component/> }/>
+          <Route path='inbox' element={ <Component/> }/>
+          <Route path='todo' element={ <Component/> }/>
+          <Route path='activity' element={ <Component/> }/>
+          <Route path='team' element={ <Component/> }/>
+          <Route path='folder/:name' element={ <Component/> }/>
+        </Route>
+      </Routes>
+    </HashRouter>
   );
 }
 
